200c965d06217a4b19e87a0a013c969d14c0d695
Alexander <mrreads@yandex.com>
2020-06-22 00:37:41 +0300

    silebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -2,31 +2,50 @@ import React from 'react';
 import styles from "./sidebar.module.scss";
 import { NavLink } from 'react-router-dom';
 import { Layout, Menu } from 'antd';
+import { ProfileOutlined, UserOutlined, TeamOutlined, TagOutlined} from '@ant-design/icons';
 
-  
 const { Header, Sider, Content } = Layout;
 
-export default function Sidebar () {
-    return (
-        <Sider>
+class Sidebar extends React.Component
+{
+    constructor()
+    {
+        super();
+        this.state = {
+            collapsed: false,
+          };
+        
+          this.onCollapse = collapsed => {
+            this.setState({ collapsed });
+          };    
+    }
+
+    render()
+    {
+        return (
+        <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
+            <h2 className={styles.logo}> CRM </h2>
             <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-                <Menu.Item key="1">
+                <Menu.Item key="1" icon={<UserOutlined />}>
                     <NavLink to="/manager" className={`${styles.menuItem}`} activeClassName={`${styles.menuItemActive}`}> Личный кабинет </NavLink>
                 </Menu.Item>
 
-                <Menu.Item key="2">
+                <Menu.Item key="2" icon={<TagOutlined />}>
                     <NavLink to="/brands" className={`${styles.menuItem}`} activeClassName={`${styles.menuItemActive}`}>Бренды</NavLink>
                 </Menu.Item>
                     
 
-                <Menu.Item key="3">
+                <Menu.Item key="3" icon={<ProfileOutlined />}>
                     <NavLink to="/goods" className={`${styles.menuItem}`} activeClassName={`${styles.menuItemActive}`}>Товары</NavLink>
                 </Menu.Item>
 
-                <Menu.Item key="4">
+                <Menu.Item key="4" icon={<TeamOutlined />}>
                     <NavLink to="/bloggers" className={`${styles.menuItem}`} activeClassName={`${styles.menuItemActive}`}>Блоггеры</NavLink>
                 </Menu.Item>
             </Menu>
-        </Sider>
-    );
+        </Sider>);
+    }
 }
+
+
+export default Sidebar;
